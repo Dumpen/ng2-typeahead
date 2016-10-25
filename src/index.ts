f41b28964b4720f1ccef4bf28c91028572d7aa32
Casper Winther Hansen <cwh@ennova.com>
2016-10-25 14:10:12 +0200

    Added so publishing to npm is a lot easier

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+﻿import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Typeahead } from "./ng2-typeahead";
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [Typeahead],
+    exports: [Typeahead]
+})
+export class TypeaheadModule {
+}
